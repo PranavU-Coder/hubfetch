60534d1a00f976f07f54b4986203a0687d41e044
PranavU-Coder <pranavu8406@gmail.com>
2026-01-06 22:54:01 +0530

    all required features scrapped, shifting to official GitHub API

--- web_scrapper.py
+++ web_scrapper.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import time
 import requests
+from datetime import datetime, timezone as tz, timedelta
 
 gh_username = input("Enter your GitHub username: ")
 profile_url = f"https://github.com/{gh_username}"
@@ -27,8 +28,14 @@ while True:
 
 # all attributes I want to extract.
 
-sum: int = 0
+sum_stars: int = 0
+repositories: int = 0
 follower_count: int = 0
+following_count: int = 0
+# contributions_this_year: int = 0
+# contributions_since_last_year: int = 0
+timezone: str = 'Not set'
+sum_starred: int = 0
 
 for repo_url in repo_links:
     
@@ -38,16 +45,58 @@ for repo_url in repo_links:
     stars_ele = soup.find('span', {'id': 'repo-stars-counter-star'})
     if stars_ele:
         stars = stars_ele.get_text().strip()
-        sum += int(stars)
+        sum_stars += int(stars)
+
+repositories = len(repo_links)
 
 response = requests.get(profile_url)
 soup = BeautifulSoup(response.text, 'lxml')
+
 followers_link = soup.find('a', href=lambda x: x and 'tab=followers' in x)
+following_link = soup.find('a', href=lambda x: x and 'tab=following' in x)
 
 if followers_link:
     followers_count = followers_link.find('span', class_='text-bold')
     if followers_count:
         follower_count = followers_count.get_text().strip()
 
-print(f"stars: {sum}")
+if following_link:
+    following_count_ele = following_link.find('span', class_='text-bold')
+    if following_count_ele:
+        following_count = following_count_ele.get_text().strip()
+
+stars_link = soup.find('a', href=lambda x: x and 'tab=stars' in x)
+if stars_link:
+    stars_counter = stars_link.find('span', class_='Counter')
+    if stars_counter:
+        sum_starred = stars_counter.get_text().strip()
+
+# content is dynamically rendered hence this is actually a bit hard to extract normally.
+
+timezone_ele = soup.find('profile-timezone')
+if timezone_ele:
+    hours_ahead = timezone_ele.get('data-hours-ahead-of-utc', '')
+
+    if hours_ahead:
+        hours = float(hours_ahead)
+        sign = '+' if hours >= 0 else '-'
+        hours_int = int(abs(hours))
+        minutes = int((abs(hours) - hours_int) * 60)
+
+        utc_now = datetime.now(tz.utc)
+        user_tz = tz(timedelta(hours=hours))
+        user_time = utc_now.astimezone(user_tz)
+        current_time = user_time.strftime('%H:%M')
+
+        timezone = f"{current_time} (UTC {sign}{hours_int:02d}:{minutes:02d})"
+
+# metrics I want to return to the end-user
+
+print("\n")
+print(f"user: {gh_username}")
+print(f"public-repositories: {repositories}")
+print(f"stars: {sum_stars}")
+print(f"starred: {sum_starred}")
 print(f"followers: {follower_count}")
+print(f"following: {following_count}")
+print(f"timezone: {timezone}")
